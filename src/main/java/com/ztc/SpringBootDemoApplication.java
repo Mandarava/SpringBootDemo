8f09204c1832ee8900153a7f40afdba15d607d37
Mandarava <569045298@qq.com>
2017-01-01 21:24:39 +0800

    springboot druid 监控配置

--- src/main/java/com/ztc/SpringBootDemoApplication.java
+++ src/main/java/com/ztc/SpringBootDemoApplication.java
@@ -2,10 +2,12 @@ package com.ztc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
+@ServletComponentScan
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
