3141d9242c82695395387bced2d99756888e4c86
Mandarava <569045298@qq.com>
2019-03-02 22:07:36 +0800

    mybatis generator

---
+++ src/main/java/com/ztc/entity/mybatis/GenerateArtifactsMain.java
@@ -0,0 +1,66 @@
+package com.ztc.entity.mybatis;
+
+import com.ztc.mapper.mybatis.CoffeeMapper;
+
+import org.joda.money.CurrencyUnit;
+import org.joda.money.Money;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by zt 2019/3/2 21:40
+ */
+@Component
+@Slf4j
+public class GenerateArtifactsMain {
+
+    @Autowired
+    private CoffeeMapper coffeeMapper;
+
+    public static void main(String[] args) throws Exception {
+        generateArtifacts();
+    }
+
+    private static void generateArtifacts() throws Exception {
+        List<String> warnings = new ArrayList<>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(GenerateArtifactsMain.class.getResourceAsStream("/generatorConfig.xml"));
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    private void playWithArtifacts() {
+        Coffee espresso = new Coffee()
+                .withName("espresso")
+                .withPrice(Money.of(CurrencyUnit.of("CNY"), 20.0))
+                .withCreateTime(new Date())
+                .withUpdateTime(new Date());
+        coffeeMapper.insert(espresso);
+
+        Coffee latte = new Coffee()
+                .withName("latte")
+                .withPrice(Money.of(CurrencyUnit.of("CNY"), 30.0))
+                .withCreateTime(new Date())
+                .withUpdateTime(new Date());
+        coffeeMapper.insert(latte);
+
+        Coffee s = coffeeMapper.selectByPrimaryKey(1L);
+        log.info("Coffee {}", s);
+
+        CoffeeExample example = new CoffeeExample();
+        example.createCriteria().andNameEqualTo("latte");
+        List<Coffee> list = coffeeMapper.selectByExample(example);
+        list.forEach(e -> log.info("selectByExample: {}", e));
+    }
+}
